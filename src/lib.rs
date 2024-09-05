8e406cf7f05f27407565fe6cfa32c5dbae9ae009
Sreyeesh <sgarime1@gmail.com>
2024-09-05 15:27:58 +0300

    Updated lib.rs with new features

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+use rand::seq::SliceRandom;
+
+#[pyfunction]
+fn get_random_os() -> &'static str {
+    let os_list = ["Linux", "Windows", "macOS", "FreeBSD", "Solaris"];
+    let os = os_list.choose(&mut rand::thread_rng()).unwrap();
+    os
+}
+
+#[pymodule]
+fn randomos(py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(get_random_os, m)?)?;
+    Ok(())
 }
